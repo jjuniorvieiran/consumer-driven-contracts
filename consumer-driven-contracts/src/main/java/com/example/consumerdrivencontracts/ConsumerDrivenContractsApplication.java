c6ec4ba69de905195f293a6b4127268f60112e3c
J. Junior <junior.jaava@gmail.com>
2023-12-27 23:29:04 +0100

    init commit

---
+++ consumer-driven-contracts/src/main/java/com/example/consumerdrivencontracts/ConsumerDrivenContractsApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumerdrivencontracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerDrivenContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerDrivenContractsApplication.class, args);
+	}
+
+}
---
+++ consumer-driven-contracts/src/test/java/com/example/consumerdrivencontracts/ConsumerDrivenContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumerdrivencontracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerDrivenContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
