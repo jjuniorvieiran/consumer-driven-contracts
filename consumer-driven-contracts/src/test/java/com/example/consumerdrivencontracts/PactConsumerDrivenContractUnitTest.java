ac7bf04bda180969929b84eb8e4fc5a44f30224d
J. Junior <junior.jaava@gmail.com>
2024-01-01 17:46:06 +0100

    PactConsumerDrivenContractUnitTest renamed

--- consumer-driven-contracts/src/test/java/com/example/consumerdrivencontracts/PactConsumerDrivenContractUnitTest.java
+++ consumer-driven-contracts/src/test/java/com/example/consumerdrivencontracts/PactConsumerDrivenContractUnitTest.java
@@ -3,7 +3,7 @@ package com.example.consumerdrivencontracts;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 
 @PactTestFor(providerName = "test_provider", hostInterface="localhost")
-class ConsumerDrivenContractsApplicationTests {
+class PactConsumerDrivenContractUnitTest {
 
 
 
