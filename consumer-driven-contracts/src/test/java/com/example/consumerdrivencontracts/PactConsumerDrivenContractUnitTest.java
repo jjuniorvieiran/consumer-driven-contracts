a987fd7bc76ecebc4fefc22334be0e6027f0e7ac
J. Junior <junior.jaava@gmail.com>
2024-01-08 21:42:16 +0100

    Defining a Contract

--- consumer-driven-contracts/src/test/java/com/example/consumerdrivencontracts/PactConsumerDrivenContractUnitTest.java
+++ consumer-driven-contracts/src/test/java/com/example/consumerdrivencontracts/PactConsumerDrivenContractUnitTest.java
@@ -1,10 +1,33 @@
 package com.example.consumerdrivencontracts;
 
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.model.RequestResponsePact;
 
-@PactTestFor(providerName = "test_provider", hostInterface="localhost")
+import java.util.HashMap;
+import java.util.Map;
+
+@PactTestFor(providerName = "test_provider", hostInterface = "localhost")
 class PactConsumerDrivenContractUnitTest {
 
+    @Pact(provider = "test_provider", consumer = "test_consumer")
+    public RequestResponsePact createPact(PactDslWithProvider builder) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
 
-
+        return builder
+                .given("test GET")
+                    .uponReceiving("GET REQUEST")
+                    .path("/pact").method("GET")
+                .willRespondWith().status(200)
+                    .headers(headers)
+                    .body("{\"condition\": true, \"name\": \"tom\"}")
+                .given("test POST")
+                    .uponReceiving("POST REQUEST")
+                    .method("POST").headers(headers)
+                    .body("{\"name\": \"Michael\"}")
+                    .path("/pact")
+                .willRespondWith().status(201).toPact();
+    }
 }
